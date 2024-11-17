7f766fd4ba7b39ef6a564e365c269150f4d52bb4
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-11-17 13:17:12 -0400

    feat: new setup (astro)

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import icon from 'astro-icon';
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+    integrations: [icon()]
+});
