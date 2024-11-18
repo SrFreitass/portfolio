930d599f9f6102ce3f551ccc355bd985a31e0e17
srfreitass <guilhermefreitasdonascimento67@gmail.com>
2024-11-17 22:43:20 -0400

    chore: add adapter in astro config (vercel)

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,11 @@
 // @ts-check
+import vercel from '@astrojs/vercel/serverless';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
 // https://astro.build/config
 export default defineConfig({
     output: 'server',
-    integrations: [icon()]
+    integrations: [icon()],
+    adapter: vercel()
 });
