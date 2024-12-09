e9ed9609debbc949ecef743bf0913def1633d9f9
SrFreitass <guilhermefreitasdonascimento67@gmail.com>
2024-12-09 00:52:45 -0400

    feat: new info and add shadcn

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,18 @@ import vercel from '@astrojs/vercel/serverless';
 import icon from 'astro-icon';
 import { defineConfig } from 'astro/config';
 
+import react from '@astrojs/react';
+
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
     prefetch: {
         prefetchAll: true
     },
     output: 'server',
-    integrations: [icon()],
+    integrations: [icon(), react(), tailwind({
+      applyBaseStyles: false,
+    })],
     adapter: vercel(),
 });
